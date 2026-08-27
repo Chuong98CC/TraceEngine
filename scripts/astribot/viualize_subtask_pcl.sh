060e49d3b6ded1b6afc7f1e77ad8badb84f8803f
chn <chuong98vt@gmail.com>
2026-08-26 21:49:05 -0700

    add code for visualizing the point cloud stream online

---
+++ scripts/astribot/viualize_subtask_pcl.sh
@@ -0,0 +1,5 @@
+python tools/astribot/visualize_subtask_stream.py \
+    --repo-id Kronze157/astri_making_coffee_vlva \
+    --data-root /data/astri_making_coffee \
+    --episode-idxes 0 \
+    --camera-idxes 4 5 --fps 30
