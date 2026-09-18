3c15060d1dc97cc714581a09e91a61d768127be9
chn <chuong98vt@gmail.com>
2026-09-18 14:20:56 -0700

    try packed normal and depth with RGB

--- scripts/general_test/module/infer_moge3.sh
+++ scripts/general_test/module/infer_moge3.sh
@@ -1,4 +1,4 @@
 python tools/general_test/module/infer_moge3.py \
     --input astribot_making_coffee_vlva_full/eps_data/sampling_points/key_frames/ep000000/subtask_00/cam_head \
     --out_dir cache/cam_head_moge \
-    --visualize
+    --visualize --save-packed --refine_steps 3
